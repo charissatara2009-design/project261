a14daf03861cdd7d2992db6a65a51f723bd1c1f2
charissatara2009-design <charissatara2009@gmail.com>
2026-03-31 13:49:06 +0700

    upadte code

---
+++ app/src/main/java/com/example/project261/HomemainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.project261
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomemainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.homemain)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/project261/SplashMainActivity.kt
+++ app/src/main/java/com/example/project261/SplashMainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.project261
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +12,11 @@ class SplashMainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.splash_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btn_mulai: Button = findViewById<Button>(R.id.btn_mulai)
+
+        btn_mulai.setOnClickListener {
+            val intent = Intent(this, Ma)
         }
     }
 }
